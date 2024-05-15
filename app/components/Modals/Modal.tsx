24205ea23e9fb2e0e8f786a0cd6058ab25a429ac
TDevtechs <torresdevtech@gmail.com>
2024-05-15 10:28:26 -0500

    Logic about Modal

--- app/components/Modals/Modal.tsx
+++ app/components/Modals/Modal.tsx
@@ -1,8 +1,70 @@
+'use client'
+
+import { use, useCallback, useEffect, useState } from "react";
+
 interface ModalProps {
-    
+    isOpen?: boolean
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disable?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+
 }
 
-const Modal = () => {
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disable,
+    secondaryAction,
+    secondaryLabel
+
+}) => {
+    const [showModal, setShowModal] = useState(isOpen);
+
+    useEffect(() => {
+        setShowModal(isOpen)
+    }, [isOpen])
+
+    const handleClose = useCallback(() => {
+        if(disable) {
+            return
+        }
+
+        setShowModal(false)
+        setTimeout(() => {
+            onClose()
+        }, 300)
+    }, [disable, onClose])
+
+    const handleSubmit = useCallback(() => {
+        if(disable){
+            return
+        }
+        onSubmit()
+    }, [disable,onSubmit])
+
+    const handleSecondaryAction = useCallback(() => {
+        if(disable || !secondaryAction){
+            return
+        }
+
+        secondaryAction()   
+    }, [disable,secondaryAction])
+
+    if(!isOpen){
+        return null
+    }
+
     return ( 
         <div>   </div>
      );
