4da4e51b4af79f7fb294ebcc6a6f385620e887d8
TDevtechs <torresdevtech@gmail.com>
2024-05-15 09:14:10 -0500

    Component protector from Hydration

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState, useEffect, Children } from "react";
+
+interface ClientOnlyProps {
+    children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({
+    children
+}) => {
+    const [hasMounted, setHasMounted] = useState(false)
+
+    useEffect(() => {
+      setHasMounted(true)
+    }, []);
+
+    if(!hasMounted){
+        return null;
+    }
+    
+
+    return (
+        <>
+        {Children}
+        </>
+    );
+}
+
+export default ClientOnly;
